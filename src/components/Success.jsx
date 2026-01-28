759e98293c04f22c56cb00c583f289c2072be4ba
zeel <sundayy119@gmail.com>
2026-01-28 19:00:12 +0530

    style1

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Success = () => {
   return (
-    <div>Payment successfull you can refresh dashboard</div>
+    <div style={{textAlign:"center"}}>Payment successfull you can refresh dashboard
+      <div style={{marginTop:'15px', cursor:"pointer"}}><Link to="/" style={{textDecoration:"none", fontWeight:"500"}}>Dashboard</Link>
+      </div>
+    
+    </div>
   )
 }
 
